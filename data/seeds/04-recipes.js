9e78bcd31605162735df89f680eaeb66a4a249cd
IsaiahFlax <flax.isaiah@gmail.com>
2020-05-28 22:32:45 -0600

    more progress

--- data/seeds/04-recipes.js
+++ data/seeds/04-recipes.js
@@ -1,13 +1,23 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('recipes').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('recipes').insert([
+        {recipe_id: '1', ingredients_id: '4', quantity: 3},
+        {recipe_id: '1', ingredients_id: '6', quantity: 2},
+        {recipe_id: '2', ingredients_id: '1', quantity: 5},
+        {recipe_id: '2', ingredients_id: '2', quantity: 1},
+        {recipe_id: '2', ingredients_id: '3', quantity: 1},
+        {recipe_id: '2', ingredients_id: '6', quantity: 2},
+        {recipe_id: '3', ingredients_id: '1', quantity: 1},
+        {recipe_id: '3', ingredients_id: '2', quantity: 1},
+        {recipe_id: '3', ingredients_id: '3', quantity: 1},
+        {recipe_id: '3', ingredients_id: '4', quantity: 1},
+        {recipe_id: '3', ingredients_id: '5', quantity: 1},
+        {recipe_id: '3', ingredients_id: '6', quantity: 2},
+
       ]);
     });
 };
